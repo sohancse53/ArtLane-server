a48892eb7e2358345a7b6042b4dafd47719d5a4a
Md. Shoyaif Rahman <sohan0020p@gmail.com>
2025-11-11 12:45:00 +0600

    filter by categiry api created

--- index.js
+++ index.js
@@ -171,6 +171,12 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/filter-by-category",async(req,res)=>{
+      const category = req.query.category;
+      const result = await artWorksCollection.find({category:category}).toArray();
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
